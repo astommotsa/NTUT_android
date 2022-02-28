4058ffc5de946b3f463ccd278b596473b4e518a0
astommotsa <63243255+astommotsa@users.noreply.github.com>
2022-02-28 21:38:07 +0800

    Add files via upload

---
+++ 108820044_HW1/app/src/main/java/com/example/a108820044_hw1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a108820044_hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "hw1 log string output");
+    }
+}
